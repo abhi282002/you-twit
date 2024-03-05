1ab014f6f24fb22dddc1561456aa9d1adaf53193
Abhishek Sharma <sharma312006@gmail.com>
2024-03-05 20:11:10 +0530

    make video Responsive

--- frontend/src/pages/DisplayVideo.tsx
+++ frontend/src/pages/DisplayVideo.tsx
@@ -18,10 +18,11 @@ const DisplayVideo = () => {
   };
   return (
     <div>
-      <div className="w-full px-4">
+      <div className="w-full h-full px-5 lg:px-12">
         <video
           src={videos.videoFile}
-          className="object-fill  rounded-tl-lg w-full rounded-tr-lg"
+          poster={videos.thumbnail}
+          className="object-fill h-[460px] w-[735px] lg:w-[900px] lg:h-[550px]  rounded-tl-lg w-full rounded-tr-lg"
           controls
           disablePictureInPicture
           controlsList="nodownload"
